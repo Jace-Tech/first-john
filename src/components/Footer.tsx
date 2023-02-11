819c56375cf92310122d071e95ddce0698ef0317
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-02-11 03:41:05 +0100

    now fully done 😋😋

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer: React.FC<FooterProps> = () => {
           <Stack spacing={1} justifyContent={"center"} alignItems={"center"} color={WHITE}>
             <Typography fontSize={".9rem"} fontFamily={"Nunito Sans, Inter, system-ui, Avenir, Helvetica, Arial, sans-serif"}>Copywrite @{year} First-John Electricals</Typography>
             <Typography fontSize={".9rem"} fontFamily={"Nunito Sans, Inter, system-ui, Avenir, Helvetica, Arial, sans-serif"}>
-              Made with ❤️ by <Link href="" target={"_blank"}>JACE</Link>
+              Made with ❤️ by <Link href="https://linktr.ee/jace_tech" target={"_blank"}>JACE</Link>
             </Typography>
           </Stack>
       </Container>
