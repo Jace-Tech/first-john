de8a453dd7cf0d279bf1dd2e1ad9d51c556bdef1
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-02-11 03:57:29 +0100

    updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,8 +13,8 @@ const Hero: React.FC<HeroProps> = () => {
     <Box py={5} minHeight={"75vh"}>
       <Container maxWidth="lg">
         <Grid container mt={5} spacing={{ xs: 10, md: 5 }}>
-          <Grid item xs={12} sm={5} md={5}>
-            <Stack spacing={5}>
+          <Grid item xs={12} md={5}>
+            <Stack spacing={5} alignItems={{ xs: "center", md: "flex-start" }}>
               <div data-aos="fade-up" data-aos-anchor-placement="center-bottom">
                 <Typography
                   fontSize={{ xs: "3.5rem", sm: "4.3rem", lg: "4.8rem" }}
@@ -22,13 +22,14 @@ const Hero: React.FC<HeroProps> = () => {
                   color={WHITE}
                   letterSpacing={3}
                   lineHeight={1.3}
+                  textAlign={{ xs: "center", md: "left" }}
                 >
-                  <span style={{ color: ORANGE }}>FIRST</span> JOHN <br /> ELECTRICALS
+                  <span style={{ color: ORANGE }}>FIRST</span> JOHN ELECTRICALS
                 </Typography>
               </div>
 
               <div data-aos="fade-up" data-aos-anchor-placement="center-bottom">
-                <Typography color={WHITE} fontSize={"1.5rem"} maxWidth={400} fontFamily={"Nunito Sans, Inter, system-ui, Avenir, Helvetica, Arial, sans-serif"}>
+                <Typography textAlign={{ xs: "center", md: "left" }} color={WHITE} fontSize={"1.5rem"} maxWidth={{ xs: "100%", md: 450}} fontFamily={"Nunito Sans, Inter, system-ui, Avenir, Helvetica, Arial, sans-serif"}>
                   At <span style={{ color: ORANGE }}>FIRST-JOHN ELECTRICALS</span> your happiness is our pride
                 </Typography>
               </div>
@@ -39,7 +40,7 @@ const Hero: React.FC<HeroProps> = () => {
             </Stack>
           </Grid>
 
-          <Grid item xs={12} sm={7} md={7}>
+          <Grid item xs={12} md={7}>
             <Box className={"light-box"}>
               <img className="light" src={light} alt="light-icon" />
             </Box>
