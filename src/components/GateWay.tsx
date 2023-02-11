df5ca9ef7a4f128503f598c86ee68c564d7c7f75
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-02-11 03:30:58 +0100

    i promise, I've fixed it

--- src/components/GateWay.tsx
+++ src/components/GateWay.tsx
@@ -1,10 +1,10 @@
-import React, { ReactNode } from "react";
+import React, { ComponentType, ReactNode } from "react";
 import { ModalGateway } from "react-images";
 
 interface Props {
-  children: ReactNode
+  children?: ReactNode
 }
 
-const FixedModalGateway: React.FC<Props> = ModalGateway;
+const FixedModalGateway: React.FC<Props> = ModalGateway as ComponentType<{}> | any
 
 export default FixedModalGateway;
